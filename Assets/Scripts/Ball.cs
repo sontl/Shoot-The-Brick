5b559749b80b5145e156e745d039328a4f767761
SonTranLam <sontl@code.edu.vn>
2020-08-30 10:23:23 +0800

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    Vector3 paddleToBallVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddle1.transform.position + paddleToBallVector;
+    }
+}
