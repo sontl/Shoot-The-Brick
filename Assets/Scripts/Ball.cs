d049c2bc5f04d23d20c4664033d96a59b5d08388
SonTranLam <sontl@mac.com>
2020-10-12 16:16:15 +0800

    update code

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    //config params
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 12f;
+    [SerializeField] AudioClip[] ballSounds;
+
+    //state
     Vector3 paddleToBallVector;
     bool hasStarted = false;
+
+    // Cached component references
+    AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,5 +48,15 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddle1.transform.position + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted) 
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+        
     }
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    Level level;
+    GameStatus gameStatus;
+
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
+
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject, 0f);
-        Debug.Log(collision.gameObject.name);
+        gameStatus.AddToScore();
+        level.BlockDestroyed();
     }
 }
